762555bd35461b090c82fe42745756ce25d4a5ea
Ahmed <Ahmed@DESKTOP-2RQQH6O>
2025-10-01 06:09:53 -0700

    Add project files.

---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace ConsoleApp5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.Write("Plase enter Number of small carpets: ");
+            int Small = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Plase enter Number of large carpets: ");
+            int Large = Convert.ToInt32(Console.ReadLine());
+
+
+            Console.WriteLine("\nPrice per small room: 25$ ");
+            Console.WriteLine("Price per large room: 35$ ");
+
+
+            int Cost = (Small * 25) + (Large * 35);
+            double Tax = Cost * 0.06;
+            double total_estimate = Cost + Tax;
+
+            Console.WriteLine($"Cost : {Cost}$");
+            Console.WriteLine($"Tax : {Tax}$");
+
+
+            Console.WriteLine("==============================================");
+            Console.WriteLine($"Total estimate: {total_estimate}$");
+            Console.WriteLine("This estimate is Value For 30 days");
+
+
+        }
+    }
+}
